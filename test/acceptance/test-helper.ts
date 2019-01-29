544efdab6fe2484f160231e6030531c19e54de13
nipunchawla <nipun@infeedo.com>
2019-01-30 00:38:03 +0530

    First Commit

---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {SetupApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new SetupApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: SetupApplication;
+  client: Client;
+}
